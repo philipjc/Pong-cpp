536da337fef1e93387ce428b97185df5c4689cd1
Philip J Cox <philipjc@users.noreply.github.com>
2022-10-31 20:17:36 +0000

    Initial commit. All Pong game files

---
+++ Ball.cpp
@@ -0,0 +1,63 @@
+#include "Ball.h"
+
+Ball::Ball(float startX, float startY)
+{
+	m_Position.x = startX;
+
+	m_Position.y = startY;
+
+	m_Shape.setSize(sf::Vector2f(10, 10));
+
+	m_Shape.setPosition(m_Position);
+}
+
+FloatRect Ball::getPosition()
+{
+	return m_Shape.getGlobalBounds();
+}
+
+RectangleShape Ball::getShape()
+{
+	return m_Shape;
+}
+
+float Ball::getXVelocity()
+{
+	return m_DirectionX;
+}
+
+/*
+	m_DirectionX has its value inverted, which will have the effect
+	of making a positive value negative and a negative value positive,
+	thereby reversing (horizontally) the direction the ball is traveling.
+*/
+void Ball::reboundSides()
+{
+	m_DirectionX = -m_DirectionX;
+}
+
+void Ball::reboundBatOrTop()
+{
+	m_DirectionY = -m_DirectionY;
+}
+
+/*
+	The reboundBottom function repositions the ball at the top center of the screen and sends it downward.
+	This is just what we want after the player has missed a ball and it has hit the bottom of the screen.
+*/
+void Ball::reboundBottom()
+{
+	m_Position.y = 0;
+	m_Position.x = 500;
+	m_DirectionY = -m_DirectionY;
+}
+
+void Ball::update(Time dt)
+{
+	// Update the ball's position
+	m_Position.y += m_DirectionY * m_Speed * dt.asSeconds();
+	m_Position.x += m_DirectionX * m_Speed * dt.asSeconds();
+
+	// Move the ball 
+	m_Shape.setPosition(m_Position);
+}
